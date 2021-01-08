910273b8c9b7a215123d9fc04dfa38fbbe026782
dlittig <david0605.littig@hotmail.de>
2021-01-08 09:42:28 +0100

    Fixed that drawer icon has wrong color

--- src/navigator/DrawerNavigator.tsx
+++ src/navigator/DrawerNavigator.tsx
@@ -14,12 +14,19 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import About from "../screens/About";
 import Settings from "../screens/Settings";
 import { useTranslation } from "react-i18next";
+import ThemeProvider from "../provider/ThemeProvider/ThemeProvider";
+import { THEMES } from "../store/constants/settingsConstants";
+import { ThemeColors } from "../theme/colors/values";
 
 const Drawer = createDrawerNavigator();
 
-const options = {
+const getOptions = (theme: string) => ({
   headerShown: true,
-};
+  headerTintColor:
+    theme === THEMES.LIGHT
+      ? ThemeColors.LightColors.text
+      : ThemeColors.DarkColors.text,
+});
 
 const headerStyle = {
   elevation: 0, // remove shadow on Android
@@ -31,73 +38,77 @@ const Navigator = () => {
   const { t } = useTranslation();
 
   return (
-    <Drawer.Navigator
-      initialRouteName={t(APP_SESSIONS)}
-      screenOptions={{ headerStyle }}
-    >
-      <Drawer.Screen
-        name={t(APP_SESSIONS)}
-        options={{
-          ...options,
-          drawerIcon: ({ focused, color, size }) => (
-            <MaterialCommunityIcons
-              name="gamepad-variant"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-        component={SessionsList}
-      />
-      <Drawer.Screen
-        name={t(APP_DRIVERS)}
-        options={{
-          ...options,
-          drawerIcon: ({ focused, color, size }) => (
-            <MaterialCommunityIcons
-              name="racing-helmet"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-        component={DriversList}
-      />
-      <Drawer.Screen
-        name={t(APP_RANDOM_MAP)}
-        options={{
-          ...options,
-          drawerIcon: ({ focused, color, size }) => (
-            <MaterialCommunityIcons name="map" color={color} size={size} />
-          ),
-        }}
-        component={RandomMap}
-      />
-      <Drawer.Screen
-        name={t(APP_SETTINGS)}
-        options={{
-          ...options,
-          drawerIcon: ({ focused, color, size }) => (
-            <MaterialCommunityIcons name="cog" color={color} size={size} />
-          ),
-        }}
-        component={Settings}
-      />
-      <Drawer.Screen
-        name={t(APP_ABOUT)}
-        component={About}
-        options={{
-          ...options,
-          drawerIcon: ({ focused, color, size }) => (
-            <MaterialCommunityIcons
-              name="information-outline"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
+    <ThemeProvider.Consumer>
+      {(theme) => (
+        <Drawer.Navigator
+          initialRouteName={t(APP_SESSIONS)}
+          screenOptions={{ headerStyle }}
+        >
+          <Drawer.Screen
+            name={t(APP_SESSIONS)}
+            options={{
+              ...getOptions(theme),
+              drawerIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons
+                  name="gamepad-variant"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+            component={SessionsList}
+          />
+          <Drawer.Screen
+            name={t(APP_DRIVERS)}
+            options={{
+              ...getOptions(theme),
+              drawerIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons
+                  name="racing-helmet"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+            component={DriversList}
+          />
+          <Drawer.Screen
+            name={t(APP_RANDOM_MAP)}
+            options={{
+              ...getOptions(theme),
+              drawerIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons name="map" color={color} size={size} />
+              ),
+            }}
+            component={RandomMap}
+          />
+          <Drawer.Screen
+            name={t(APP_SETTINGS)}
+            options={{
+              ...getOptions(theme),
+              drawerIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons name="cog" color={color} size={size} />
+              ),
+            }}
+            component={Settings}
+          />
+          <Drawer.Screen
+            name={t(APP_ABOUT)}
+            component={About}
+            options={{
+              ...getOptions(theme),
+              drawerIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons
+                  name="information-outline"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+        </Drawer.Navigator>
+      )}
+    </ThemeProvider.Consumer>
   );
 };
 
