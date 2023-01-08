eda08853a6238067683c7a08199bd309488acab7
dlittig <david0605.littig@hotmail.de>
2023-01-08 21:16:43 +0100

    test: ✅ Fixed that tests didnt run properly

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: "jest-expo",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!(react-native" +
+      "|@react-native" +
       "|react-native-paper" +
       "|react-native-iphone-x-helper" +
       "|redux-persist-expo-filesystem" +
